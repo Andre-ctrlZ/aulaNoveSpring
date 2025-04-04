d56a18f3958a9ea2d13337698aaddd14bebdab27
Andre-ctrlZ <luiace2003@gmail.com>
2025-04-04 20:40:50 -0300

    Novo. Adicionando novas exceptions.

---
+++ aulaNoveSpring/aulaNoveSpring/aulaNoveSpring/src/main/java/com/example/aulaNoveSpring/AulaNoveSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.aulaNoveSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaNoveSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaNoveSpringApplication.class, args);
+	}
+
+}
---
+++ aulaNoveSpring/aulaNoveSpring/aulaNoveSpring/src/test/java/com/example/aulaNoveSpring/AulaNoveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aulaNoveSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaNoveSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
